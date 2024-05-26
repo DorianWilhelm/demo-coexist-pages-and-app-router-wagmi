eb5b80988b1f3cd5166b47df67067d557da04e8f
Dorian Wilhelm <wilhelm.dorian@pm.me>
2024-05-26 10:38:17 +0200

    feat: Successful demo with abstracted query component

--- src/pages/pages-router.tsx
+++ src/pages/pages-router.tsx
@@ -1,27 +1,10 @@
 "use client";
-import { useQuery } from "@tanstack/react-query";
+import { QueryTestComponent } from "@/components/QueryTestComponent";
 
 export default function Page() {
-  const { isPending, error, data } = useQuery({
-    queryKey: ["repoData"],
-    queryFn: () =>
-      fetch("https://api.github.com/repos/TanStack/query").then((res) =>
-        res.json()
-      ),
-  });
-
-  if (isPending) return "Loading...";
-
-  if (error) return "An error has occurred: " + error.message;
   return (
     <div>
-      <div>
-        <h1>{data.name}</h1>
-        <p>{data.description}</p>
-        <strong>👀 {data.subscribers_count}</strong>{" "}
-        <strong>✨ {data.stargazers_count}</strong>{" "}
-        <strong>🍴 {data.forks_count}</strong>
-      </div>
+      <QueryTestComponent />
     </div>
   );
 }
